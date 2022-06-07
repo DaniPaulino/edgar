ffbda8a333220b69af846b2aa9dbc34b859a60c3
EdgarNahum <edgar.nmartins95@gmail.com>
2022-06-07 17:34:54 +0000

    v1

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 import cloudpickle
+from flask import Flask, render_template, request
 
 with open('model.pkl', 'rb') as file_in:
-  model_loaded = cloudpickle.load(file_in)
+  model = cloudpickle.load(file_in)
 
-frase = input('Digite uma frase: ')
+app = Flask(__name__)
 
-if model_loaded.predict([frase])[0] == 0:
-    print('Comentário negativo')
-else:
-    print('Comentário positivo')
+@app.route('/')
+def index():
+  return render_template('index.html', nome='Análise de Atrito')
+
+@app.route('/Atrition', methods=['POST'])
+def predicao():
+  Age = request.form['Age']
+  YearsAtCompany = request.form['YearsAtCompany']
+  Department = request.form['Department']
+  JobSatisfaction = request.form['JobSatisfaction']
+  MaritalStatus = request.form['MaritalStatus']
+  WorkLifeBalance = request.form['WorkLifeBalance']
+ 
+
+  array=[[str(Age), str(YearsAtCompany), str(Department), str(JobSatisfaction), str(MaritalStatus), str(WorkLifeBalance)]]
+
+  Atrition = model.predict(array)
+
+  return render_template('resposta.html', predicao=predicao[0])
+
+app.run(debug=True)
+
+# pip install -r requirements.txt (instala as bibliotecas)
+# python app.py (para executar)
